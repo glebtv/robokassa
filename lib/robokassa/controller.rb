8333b1a98f53d468b28bff722fb4cd18b9685eee
glebtv <glebtv@gmail.com>
2013-12-08 19:11:48 +0400

    rails 3 compatible

--- lib/robokassa/controller.rb
+++ lib/robokassa/controller.rb
@@ -1,7 +1,7 @@
 module Robokassa::Controller
   extend ActiveSupport::Concern
   included do
-    skip_before_action :verify_authenticity_token
+    skip_before_filter :verify_authenticity_token
   end
   
   def notify
