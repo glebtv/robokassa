c48cc92c5e3ae4e82030dac03a9e30ef2813707a
Gleb Tv <glebtv@gmail.com>
2019-02-05 18:35:07 +0300

    rails update

--- lib/robokassa/controller.rb
+++ lib/robokassa/controller.rb
@@ -1,9 +1,9 @@
 module Robokassa::Controller
   extend ActiveSupport::Concern
   included do
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
   end
-  
+
   def notify
     if params[:token] != Robokassa.interface.token
         raise Robokassa::InvalidToken.new
